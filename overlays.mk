b2e8290010bb419899ccd359edc7be031b49581e
kdrag0n <danny@kdrag0n.dev>
2021-12-15 11:51:14 +0000

    [SQUASH]overlays: import some overlays from proton aosp
    overlay: core: Use accent color for progress bar background
    This matches the stock Pixel style and makes progress bars look more
    integrated, since the active (foreground) part is using the accent
    color.
    
    overlay: SystemUI: Remove power menu shadow
    overlay: SystemUI: Use default surface colors for power menu buttons …
    overlay: SystemUI: Follow light/dark theme in power menu …
    overlay: SystemUI: Use Monet colors for power menu …
    overlay: SystemUI: Make emergency button less prominent in global act
    overlay: core: Add chroma to light surface highlight color
    device_config: Show setting to disable location indicators

--- overlays.mk
+++ overlays.mk
@@ -1,12 +1,12 @@
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += vendor/overlay/common
+DEVICE_PACKAGE_OVERLAYS += vendor/overlays/common
 
 # Allow overlays to be excluded from enforcing RRO
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlay/common
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlays/common
 
 # Pixel sysconfig from Pixel XL (Photos)
 PRODUCT_COPY_FILES += \
-    vendor/overlay/prebuilt/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml \
+    vendor/overlays/prebuilt/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml \
 
 # RRO Overlays
 PRODUCT_PACKAGES += \
