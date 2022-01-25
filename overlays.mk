2c60d437f53738069a6f9359995efafc05c7427c
Aryan Gupta <guptaaryan189@gmail.com>
2022-01-25 09:02:32 +0000

    drop gphotos haxx

--- overlays.mk
+++ overlays.mk
@@ -4,10 +4,6 @@ DEVICE_PACKAGE_OVERLAYS += vendor/overlays/common
 # Allow overlays to be excluded from enforcing RRO
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlays/common
 
-# Pixel sysconfig from Pixel XL (Photos)
-PRODUCT_COPY_FILES += \
-    vendor/overlays/prebuilt/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml \
-
 # RRO Overlays
 PRODUCT_PACKAGES += \
     CustomPixelLauncherOverlay \
