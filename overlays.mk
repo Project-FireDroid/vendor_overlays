127ebc379de74812147e4b7fb4012ce81d3bdf71
heisinbug <guptaaryan189@gmail.com>
2023-04-03 16:14:47 +0530

    Clean Overlays

--- overlays.mk
+++ overlays.mk
@@ -2,11 +2,11 @@
 DEVICE_PACKAGE_OVERLAYS += vendor/overlays/common
 
 # Allow overlays to be excluded from enforcing RRO
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlays/common
+#PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlays/common
 
 # RRO Overlays
-PRODUCT_PACKAGES += \
-    CustomPixelLauncherOverlay
+#PRODUCT_PACKAGES += \
+#    CustomPixelLauncherOverlay
 
 #Fonts
-$(call inherit-product, vendor/overlays/fonts.mk)
+#$(call inherit-product, vendor/overlays/fonts.mk)
